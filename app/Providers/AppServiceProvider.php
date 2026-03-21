c077731819f1042c48feebd8d77146411a7adef6
JOHNARVINTUM <202310149@fit.edu.ph>
2026-03-22 01:04:39 +0800

    Force production root URL for asset generation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
+            URL::forceRootUrl((string) config('app.url'));
             URL::forceScheme('https');
         }
     }
